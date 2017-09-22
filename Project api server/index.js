58851207abecae18d1fb89d5a9acf02bfb6df207
essamalsaloum <es.alsaloum@gmail.com>
2017-09-22 11:54:01 +0200

    api server added

---
+++ Project api server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mysql = require('mysql');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const router = require('./routers');
+const app = express();
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
---
+++ Project api server/routers.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync("config-secret.json"));
+// DB Setup
+// Create connection
+const db = mysql.createConnection({
+    host: config.host,
+	user: config.user,
+	password: config.password,
+	port: config.port,
+    database: config.database
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+module.exports = function(app) {
+    // Select posts
+    app.get('/', (req, res) => {
+        let sql = `SELECT tag.name  FROM tag;`;
+        let query = db.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            res.send(JSON.stringify(result));
+        });
+    });
+    // Select more post
+    app.get('/Search?', (req, res) => {
+        let names = req.query;
+        names.length > 0 ? names : false;
+        let tags = names.name.map(e => `${e}`);
+        tags = mysql.escape(tags);
+        console.log(tags);
+        //IN(' + tags.map(e => '?') +')
+        //let sql = `SELECT org.name   FROM org INNER JOIN org_has_tag ON org.id = org_has_tag.org_id where org_has_tag.tag_id = ? ;`;
+        let sql = `SELECT *
+        FROM tag INNER JOIN ((org INNER JOIN contact ON org.id = contact.org_id) INNER JOIN org_has_tag ON org.id = org_has_tag.org_id) ON tag.id = org_has_tag.tag_id
+        WHERE tag.name IN (${tags});`;
+        let query = db.query(sql,tags, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            res.send(JSON.stringify(result));
+        });
+    });
+}
